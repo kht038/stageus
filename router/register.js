d4931b7bf9e155cf8efbc2ae168862099eb29fab
Ubuntu <ubuntu@ip-172-31-38-90.ap-northeast-2.compute.internal>
2022-08-29 09:59:39 +0000

    register done

--- router/register.js
+++ router/register.js
@@ -6,8 +6,23 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../page/register.html'));
 });
 
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+  try{
+    const createdUser = await User.create({
+      user_role: req.body.role,
+      user_id: req.body.id,
+      user_password: req.body.pw,
+      user_name: req.body.name,
+      user_email: req.body.email,
+      user_phone: req.body.phone
+    });
+    await createdUser.save();
+    res.redirect('/');
+  }
+  catch(err){
+    res.send(err);
+  }
+  
 });
 
 module.exports = router;
